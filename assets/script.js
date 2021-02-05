b36556345b0c4b381cf8962e2d6b04b379383c50
b00kbaker <caimmurphy@gmail.com>
2021-02-04 20:27:00 -0700

    Dayjs connected and basic pseudo code

---
+++ assets/script.js
@@ -0,0 +1,17 @@
+var currentDate = dayjs().date();
+console.log(currentDate);
+
+// Global variables
+// Current city
+// This week
+// Weather Array=[Sunny, Overcast, Rain, Snow, Windy,]
+
+// When the page loads search localStorage to see last city viewed and view it's stats + list up to 5(?) past searches. If nothing in localStorage view basic page format or set a city as a default? 
+
+// View current date (DD/MM/YYYY)
+
+// When city is searched run weather API (does it have everything for the map side of things too?)
+
+// Display in main frame (think jumbotron) the city name/date/current day's weather (temperature/humidity/wind speed/UV index)
+
+// In 5 day cards display next days in order of proper sequence {card1/card2/card3/card4/card5} correct image/date (DD/MM/YYYY)/temperature/humidity
