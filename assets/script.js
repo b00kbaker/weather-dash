c746eca5fbaf6f07987dca73fc83eab892370e43
b00kbaker <caimmurphy@gmail.com>
2021-03-01 22:04:44 -0700

    Set local strorage ot string not array, displaying last searched but need to create list with click events

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,9 @@
 var currentDate = dayjs();
 var myKey = "1c142e1c318254dff9cf9240d0f423c4";
 var pastCities = document.getElementsByTagName("li");
-var cityList = document.getElementsByTagName("ul");
+// var cityList = document.getElementsByTagName("ul");
+var historyCities=[];
+var enterCity;
 
 document.getElementById("current-date").textContent = currentDate.format(
   "MM/DD/YYYY"
@@ -9,8 +11,13 @@ document.getElementById("current-date").textContent = currentDate.format(
 
 document.getElementById("enter-city").addEventListener("click", function () {
   var searchCity = document.getElementById("city-search");
-  var enterCity = searchCity.value;
+  enterCity = searchCity.value;
+  createWeather();
+  
+}
+);
 
+function createWeather(){
   var requestOptions = {
     method: "GET",
     redirect: "follow",
@@ -50,7 +57,13 @@ document.getElementById("enter-city").addEventListener("click", function () {
         .catch((error) => console.log("error", error));
     })
   );
-});
+
+  historyCities.push(enterCity);
+  localStorage.setItem('cities', enterCity);
+
+
+}
+
 
 function displayCurrent(tempData) {
   var currentTemp = tempData.main.temp;
@@ -67,11 +80,12 @@ function displayCurrent(tempData) {
   var currentCity = tempData.name;
   document.getElementById("city-name").textContent = currentCity;
   return;
-}
+};
+
 function displayUVI(onedata) {
   var currentUVI = onedata.current.uvi;
   document.getElementById("today-uvi").textContent = currentUVI;
-}
+};
 
 function displayForescast(onedata) {
   var oneTemp = onedata.daily[0].temp.day;
@@ -104,8 +118,15 @@ function displayForescast(onedata) {
   var fiveHumid = onedata.daily[4].humidity;
   document.getElementById("humid-5").textContent = fiveHumid;
 
+};
+
+var localStoragecity= localStorage.getItem('cities');
+
+if(localStoragecity){
+  var savedCity= localStorage.getItem('cities');
+  console.log("hi",savedCity);
+  enterCity= savedCity;
+  createWeather();
+  var cityList = document.getElementsByTagName("ul");
 }
 
-// When the page loads search localStorage to see last city viewed and view it's stats + list up to 5(?) past searches. If nothing in localStorage view basic page format or set a city as a default?
-
-// In 5 day cards display next days in order of proper sequence {card1/card2/card3/card4/card5} correct image/date (DD/MM/YYYY)/temperature/humidity
